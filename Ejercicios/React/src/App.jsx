2d22bd78b442b75206aa474e59fda2920436fa16
Daniel <147053568+DaniDiaz-commits@users.noreply.github.com>
2025-02-14 00:10:58 +0100

    proyecto react

---
+++ Ejercicios/React/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <article style={{ display:'flex', alignItems: 'center', color: '#fff'}}>
+      <header>
+        <img src="https://unavatar.io/midudev" alt="Avatar de midu" />
+        <div>
+          <strong>Miguel Angel Duran</strong>
+          <span>@midudev</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  )
+}
+
+export default App
